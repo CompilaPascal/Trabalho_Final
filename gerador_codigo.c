7ee60641b299c1b22443f0307b2b9be0e9314c01
Cecilia <ceciquaresma2014@gmail.com>
2024-07-03 09:53:00 -0300

    gerador de codigo inicial

---
+++ gerador_codigo.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void inpp(){
+    programa();
+    printf("INPP\n");
+}
+
+void crct(){}
+
+void soma(){}
+
+void mult(){}
+
+void subt(){}
+
+void divi(){}
+
+void invr(){}
+
+void nega(){}
+
+void conj(){}
+
+void disj(){}
+
+void cmme(){}
+
+void cmma(){}
+
+void cmig(){}
+
+void cmdg(){}
+
+void cmag(){}
+
+void cmeg(){}
+
+void dsvf(){}
+
+void dsvs(){}
+
+void nada(){}
+
+void amem(){}
+
+void dmem(){}
+
+void enrt(){}
+
+void enpr(){}
+
+void rtpr(){}
+
+void chpr(){}
+
+void crvl(){}
+
+void cren(){}
+
+void armz(){}
+
+void armi(){}
+
+void impr(){}
+
+void leit(){}
+
+
+
+
+int main(){
+
+}
